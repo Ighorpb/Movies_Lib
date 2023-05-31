f9c21a55d7a3d43e5cf24fe665ec5e3c00bf0d1d
Ighorpb <pb.ighor@gmail.com>
2023-05-31 09:39:56 -0300

    Projeto Finalizado!

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,12 @@
 import { Outlet } from "react-router-dom";
-import {NavBar} from './components/Navbar'
+import Navbar from './components/Navbar'
 
 import "./App.css";
 
 function App() {
   return (
     <div className="App">
-      <NavBar>
-      <h2>Movies Lib</h2>
-      </NavBar>
+      <Navbar />
       <Outlet />
     </div>
   );
